7e842d009b2ee8d6c4c5c31693148d8abdaaed14
Alberto Lara <albertojoselara39@gmail.com>
2026-08-18 03:10:57 -0400

    fix: restaurar vite.config.js dañado

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-c:\Users\Vinotinto2026\Documents\spafit-v2import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
